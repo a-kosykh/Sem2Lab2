49b18e8fad791a6ec4425493c18896b816617067
Alex <kik9399@mail.ru>
2017-02-26 12:02:49 +0300

    added tests

---
+++ tests/source/init.cpp
@@ -0,0 +1,10 @@
+#include <testclass.h>
+#include <catch.hpp>
+
+SCENARIO ("getDollarPriceCheck", "[USDcheck]"){
+    double price = 180;
+    double dollar = 60;
+
+    double Book::getDollPrice(double dollar)
+        REQUIRE ( rv == 3 );
+}
