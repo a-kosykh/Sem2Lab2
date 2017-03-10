5d834aed343f3441a944fa2627c4a2de66fd8d06
Aleksandr Kosykh <akosoj@users.noreply.github.com>
2017-03-10 19:17:27 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -2,9 +2,9 @@
 #include <catch.hpp>
 
 SCENARIO ("getDollarPriceCheck", "[Book::getDollPrice]"){
-    Book obj = new Book("asdasd", 180, 2);
+    Book *obj = new Book("asdasd", 180, 2);
 	double x = 60;
 
-    double rv = obj.getDollPrice(x);
+    double rv = obj->getDollPrice(x);
         REQUIRE ( rv == 3 );
 }
